fe82c07f0877ff7edbe3a9fe8beee020b464a2e5
Curmei Ion <ion.xd@bk.ru>
2023-09-23 19:20:36 +0300

    am mai adaugat ceva in fisier:

--- Lectia01/lesson01.py
+++ Lectia01/lesson01.py
@@ -1,3 +1,14 @@
 # acesta este un comentariu
 # pentru a printa ceva la ecran folosim comanda print("aici scrim tot ce vrem sa afiseze la ecran sau consola")
 print("Primul program pe python")
+# variabile in python sunt de mai multe tipuri
+# crearea variabilelor are loc in urmatoarea forma: nume variabila = valoare
+# variabila de tip int
+numar = 1
+# variabila de tip float 
+numar1 = 2.34
+# variabila de tip str
+nume = " Ion "
+# pentru a vedea tipul variabilei introducem comanda print(type(denumirea variabilei))
+print("Variabila numar este de tip:")
+print(type(numar))
