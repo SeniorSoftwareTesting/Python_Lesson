9a620a0f5d8e0cfaa4ccc736af7b0d264db28dc3
Curmei Ion <ion.xd@bk.ru>
2023-09-23 19:45:19 +0300

    am creat un nou fisier

---
+++ Lectia01/Operatii.py
@@ -0,0 +1,14 @@
+# operatii in python 
+#1 adunare scadere , inmultire, impartire
+nr1 = 2
+nr2 = 4
+nr3 = 5
+adunare = nr1 + nr2
+scadere = nr2 - nr1
+inmultire = nr1 * nr2 * nr3
+impartire = nr2 / nr1
+# ridicarea la putere 
+exponent =  nr3 ** nr2
+
+# calcularea radicalului 
+radical = nr2 ** 0.5
--- Lectia01/lesson01.py
+++ Lectia01/lesson01.py
@@ -11,4 +11,28 @@ numar1 = 2.34
 nume = " Ion "
 # pentru a vedea tipul variabilei introducem comanda print(type(denumirea variabilei))
 print("Variabila numar este de tip:")
-print(type(numar))
+print(type(numar)) # output <class 'int'>
+
+
+
+
+# schimbarea variabilei prin a 3-a variabila
+a = 2
+b = 3
+temp1 = a
+print("a este: ",a,", b este: ",b,", temp1 este: ",temp1)
+a = b
+b = temp1
+print("a este: ",a,", b este : ",b)
+
+
+
+# mai putem crea mai multe variabile intr-o singura linie
+x,y,z=4,5,6
+# aici x=4, y=5, z=6
+
+#putem sa schimbam valoarea a 2-a variabile fara a 3-a variabila
+nr = 3
+nr1 = 45
+nr,nr1 = nr1,nr
+print("nr este: ",nr,", nr1 este: ",nr1)
